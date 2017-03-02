5d30ee9248a40157e619fb8b84c2366e40694bff
jrobey <robeyjj@yahoo.com>
2017-03-02 11:30:51 -0800

    First upload. Up to ch3 done

---
+++ Robey_Project_01/Procedural Content Generation for Unity Game Development_Code/Roguelike/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic; 		//Allows us to use Lists.
+using Random = UnityEngine.Random; 		//Tells Random to use the Unity Engine random number generator.
+
+	
+public class BoardManager : MonoBehaviour
+{
+	// Using Serializable allows us to embed a class with sub properties in the inspector.
+	[Serializable]
+	public class Count
+	{
+		public int minimum; 			//Minimum value for our Count class.
+		public int maximum; 			//Maximum value for our Count class.
+		
+		
+		//Assignment constructor.
+		public Count (int min, int max)
+		{
+			minimum = min;
+			maximum = max;
+		}
+	}
+    public int columns = 5;
+    public int rows = 5;
+    public GameObject[] floorTiles;
+    private Transform boardHolder;
+    private Dictionary<Vector2, Vector2> gridPositions = new Dictionary<Vector2, Vector2>();
+    public GameObject[] wallTiles;
+
+    public void BoardSetup()
+    {
+        boardHolder = new GameObject("Board").transform;
+
+        for (int x = 0; x < columns; x++)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                gridPositions.Add(new Vector2(x, y), new Vector2(x, y));
+
+                GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+    private void addTiles(Vector2 tileToAdd)
+    {
+        if (!gridPositions.ContainsKey(tileToAdd))
+        {
+            gridPositions.Add(tileToAdd, tileToAdd);
+            GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+            GameObject instance = Instantiate(toInstantiate, new Vector3(tileToAdd.x, tileToAdd.y, 0f), Quaternion.identity) as GameObject;
+
+            instance.transform.SetParent(boardHolder);
+
+            //Choose at random a wall tile to lay
+            if (Random.Range(0,3) == 1)
+            {
+                toInstantiate = wallTiles[Random.Range(0, wallTiles.Length)];
+                instance = Instantiate(toInstantiate, new Vector3(tileToAdd.x, tileToAdd.y, 0f), Quaternion.identity) as GameObject;
+                instance.transform.SetParent(boardHolder);
+            }
+        }        
+    }
+
+    public void addToBoard(int horizontal, int vertical)
+    {
+        if (horizontal == 1)
+        {
+            //Check if tiles exist
+            int x = (int)Player.position.x;
+            int sightX = x + 2;
+            for (x +=1; x <= sightX; x++)
+            {
+                int y = (int)Player.position.y;
+                int sightY = y + 1;
+                for (y -= 1; y <= sightY; y++)
+                {
+                    addTiles(new Vector2(x, y));
+                }
+            }
+        }
+        else if (horizontal == -1)
+        {
+            int x = (int)Player.position.x;
+            int sightX = x - 2;
+            for(x -= 1; x >= sightX; x--)
+            {
+                int y = (int)Player.position.y;
+                int sightY = y + 1;
+                for (y -= 1; y <= sightY; y++)
+                {
+                    addTiles(new Vector2(x, y));
+                }
+            }
+        }
+        else if (vertical == 1)
+        {
+            int y = (int)Player.position.y;
+            int sightY = y + 2;
+            for (y += 1; y <= sightY; y++)
+            {
+                int x = (int)Player.position.x;
+                int sightX = x + 1;
+                for (x -= 1; x <= sightX; x++)
+                {
+                    addTiles(new Vector2(x, y));
+                }
+            }
+        }
+        else if (vertical == -1)
+        {
+            int y = (int)Player.position.y;
+            int sightY = y - 2;
+            for (y -= 1; y >= sightY; y--)
+            {
+                int x = (int)Player.position.x;
+                int sightX = x + 1;
+                for (x -= 1; x <= sightX; x++)
+                {
+                    addTiles(new Vector2(x, y));
+                }
+            }           
+        }
+    }
+}
